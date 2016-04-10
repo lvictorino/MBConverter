fdc9eb8cff56aa07065a7341b1f08906d2c7c57f
Laurent Victorino <lvictorino@monkeymoon.net>
2016-04-10 02:24:16 +0200

    Add world coordinates and offsets (in metter) to props.json

--- mbconverter.py
+++ mbconverter.py
@@ -3,6 +3,31 @@ import sys
 import json
 from shutil import copyfile, rmtree
 
+#Extracted from http://demo.business-geografic.com/osm/tilemap.xml
+zoom_upp = [ 156543.033928,
+             78271.516964,
+             39135.758482,
+             19567.879241,
+             9783.939621,
+             4891.969810,
+             2445.984905,
+             1222.992453,
+             611.496226,
+             305.748113,
+             152.874057,
+             76.4370282714844,
+             38.2185141357422,
+             19.1092570678711,
+             9.55462853393555,
+             4.77731426696777,
+             2.38865713348389,
+             1.19432856674194,
+             0.59716428337097,
+             0.29858214169741 ]
+
+tile_size = 256
+
+# Returned a directory list, sorted alphabetically, for a given path
 def get_sorted_directories(path=''):
     complete_path = os.getcwd() + '/'
     if path is not '':
@@ -11,6 +36,8 @@ def get_sorted_directories(path=''):
     directories=sorted([d for d in os.listdir(complete_path) if os.path.isdir(complete_path + d) and d != 'dump' ])
     return directories
 
+# Convert int to string and add up to two zeros before if needed
+# 42 will become '042', 7 will become '007'... etc.
 def add_zero(val):
     val_str = str(val)
     if val < 10:
@@ -19,6 +46,22 @@ def add_zero(val):
         val_str = '0' + str(val)
     return val_str
     
+# Convert the tile index (x,y) into world coordinates (in meter)
+def compute_world_position(z, x, y):
+    upp = zoom_upp[z] * tile_size
+    world_x = upp * x + upp / 2
+    world_y = upp * y + upp / 2
+    return { 'zoom': z, 'coord_x': world_x, 'coord_y': world_y, 'offset': upp }
+
+# Convert a dictionary to JSON and write it into a file at a given path
+def save_json(path, props):
+    f = open(path, 'w')
+    f.write( json.dumps(props) )
+    f.close()
+
+
+
+
 
 if os.path.exists('dump') :
     rmtree('dump')
@@ -40,13 +83,13 @@ for zoom_dir in get_sorted_directories():
             y_str = add_zero(y)
             # Copy and rename original file to dump directory
             copyfile( path + f, 'dump/' + zoom_dir + '/' + x_str + "_" + y_str + ".png" )
+            if x == 0 and y == 0:
+                world_pos = compute_world_position(int(zoom_dir), int(tile_dir), int(os.path.splitext(f)[0]))
             y = y - 1
         x = x + 1
 
     # Write props (x;y) as json file.
-    f = open('dump/'+zoom_dir+'/props.json', 'w')
-    f.write( json.dumps({'x': str(x), 'y': len(png_files)}) )
-    f.close()
+    save_json('dump/'+zoom_dir+'/props.json', dict( { 'count_x': x, 'count_y': len(png_files) }.items() + world_pos.items() ) )
     file_count = x * len(png_files)
     print str( file_count ) + ' files written.'
     total += file_count
